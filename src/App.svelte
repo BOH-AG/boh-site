3f10473142f72790537bffde4ef80cf472ec77e6
sussy-balls-impostor <eo2dog192@gmail.com>
2022-08-09 20:55:00 +0200

    page

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Header from './head/head.svelte';
+  import Anchor from './elements/anchor.svelte';
+  import It from './homepage/it.svelte';
+</script>
+
+<svelte:head>
+  <title>bruh bruh</title>
+</svelte:head>
+
+<main>
+  <Header page={"home page"}/>
+  <It/>
+  <div style="height:1000px">
+
+  </div>
+</main>
+
+<style>
+  
+</style>
---
+++ src/elements/anchor.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let url;
+    export let text;
+</script>
+
+<a href={url}>{text}</a>
+
+<style>
+a {
+  font-family: Inter, sans-serif;
+  font-weight: 400;
+  color: #eef4ed;
+  text-decoration: underline;
+  text-decoration-color: #2892d7;
+  text-underline-offset: 2px;
+  transition-duration: 300ms;
+  transition-timing-function: ease;
+}
+a:hover {
+  color: #2892d7;
+  text-decoration-color: #2891d700;
+}
+
+</style>
