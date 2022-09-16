bce264fa39c380a04edba4458350e08bcecd7dd2
sussy-balls-impostor <eo2dog192@gmail.com>
2022-09-16 15:00:33 +0200

    rollup

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,30 @@
 <script>
-  import Header from './head/head.svelte';
-  import Anchor from './elements/anchor.svelte';
-  import It from './homepage/it.svelte';
+	export let name;
 </script>
 
-<svelte:head>
-  <title>bruh bruh</title>
-</svelte:head>
-
 <main>
-  <Header page={"home page"}/>
-  <It/>
-  <div style="height:1000px">
-
-  </div>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
-  
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
 </style>
